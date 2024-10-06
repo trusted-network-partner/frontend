eeabfbf2678f82642c395718d8a39fc1522879e0
Roman <romazelenskiy2000@gmail.com>
2024-10-06 23:55:34 +0300

    fix styles with data

--- components/headers/menu.vue
+++ components/headers/menu.vue
@@ -130,11 +130,6 @@ export default {
           const spaceAbove = triggerRect.top;
           const spaceRight = window.innerWidth - triggerRect.right;
           const spaceLeft = triggerRect.left;
-
-          console.log(
-            spaceBelow < dropdownHeight && spaceAbove > dropdownHeight
-          );
-
           if (spaceBelow < dropdownHeight && spaceAbove > dropdownHeight) {
             this.isOpenUp = true;
           } else {
@@ -173,65 +168,4 @@ export default {
 };
 </script>
 
-<style lang="scss" >
-@import "~/assets/css/header/account.scss";
-// .select-container {
-//   position: relative;
-// }
-
-// .select-dropdown {
-//   position: absolute;
-//   top: 100%;
-//   left: 0;
-//   z-index: 1000;
-// }
-
-// .select-container.open-up .select-dropdown {
-//   top: auto;
-//   bottom: 100%;
-// }
-
-// .select-container.align-right .select-dropdown {
-//   left: auto;
-//   right: 0;
-// }
-
-// .nuxt-link-exact-active {
-//   font-weight: bold;
-//   color: $baseOrange;
-// }
-
-// .account-wrap > div {
-//   cursor: pointer;
-//   width: 50px;
-//   height: 50px;
-//   display: flex;
-//   align-items: center;
-//   justify-content: center;
-//   border-radius: $border-5;
-//   background: $fifthlyGray;
-//   position: relative;
-// }
-// .account {
-//   &__name {
-//     font-weight: 600;
-//     font-size: 18px;
-//   }
-//   &__icon {
-//     background: $baseWhite;
-//     border-radius: 2px;
-//     width: 15px;
-//     height: 15px;
-//     position: absolute;
-//     bottom: 0px;
-//     right: -2px;
-//     display: flex;
-//     align-items: center;
-//     justify-content: center;
-//     svg path {
-//       fill: $baseBlack;
-//     }
-//   }
-// }
-
-</style>
+<style lang="scss"></style>
